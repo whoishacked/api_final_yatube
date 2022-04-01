f6b8166458eb6a0da763bc0a91239bf0cfdb0fe5
Andrew Kutuzov <britvill@gmail.com>
2022-04-02 00:11:03 +0300

    First deploy

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -2,18 +2,16 @@ from rest_framework import permissions
 
 
 class OwnerOrReadOnly(permissions.BasePermission):
-
+    """Only owner can edit or delete object permission."""
     def has_permission(self, request, view):
-        return (
-                request.method in permissions.SAFE_METHODS
-                or request.user.is_authenticated
-            )
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
 
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
 
 
 class ReadOnly(permissions.BasePermission):
-
+    """Only read permission."""
     def has_permission(self, request, view):
         return request.method in permissions.SAFE_METHODS
